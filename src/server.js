ee83518d259b444f640a76769ff49222ead5aa39
andres <andresiturria96@gmail.com>
2021-11-08 21:41:52 +0100

    añadidos urls al server

--- src/server.js
+++ src/server.js
@@ -16,9 +16,40 @@ app.engine( 'html', nunjucks.render ) ;
 app.set( 'view engine', 'html' ) ;
 
 app.get('/', function(req, res) {
-    res.render('index.html');
+  res.render('index.html');
+});
+
+app.get('/dashboard', function(req, res) {
+  res.render('dashboard.html');
+});
+
+app.get('/registrarse', function(req, res) {
+  res.render('registrarse.html');
+});
+
+app.get('/listadeadopcion', function(req, res) {
+  res.render('listadeadopcion.html');
+});
+
+app.get('/consejos', function(req, res) {
+  res.render('consejos.html');
+});
+
+app.get('/catstitting', function(req, res) {
+  res.render('catsitting.html');
+});
+
+app.get('/incidencias', function(req, res) {
+  res.render('incidencias.html');
+});
+
+app.get('/perfil', function(req, res) {
+    res.render('perfil.html');
+  });
+
+app.get('/perfil/solicitudcatsitter', function(req, res) {
+    res.render('solicitudcatsitter.html');
   });
 
 app.listen( port ) ;
 console.log( 'Listening on port %s...', port ) ;
-
