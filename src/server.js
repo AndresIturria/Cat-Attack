c268898290fac668c701a538196d8f6e5ab81822
andres <andresiturria96@gmail.com>
2021-11-08 16:14:03 +0100

    añadido a server configuracion de nunjucks y express

--- src/server.js
+++ src/server.js
@@ -1,14 +1,24 @@
 const http = require('http')
 var express = require('express');
 var app = express();
-require('nunjucks')
+var nunjucks = require('nunjucks')
+
+var port = process.env.PORT || 9000 ;
+
 
 nunjucks.configure('static', {
     autoescape: true,
     express: app
 });
 
+// Set Nunjucks as rendering engine for pages with .html suffix
+app.engine( 'html', nunjucks.render ) ;
+app.set( 'view engine', 'html' ) ;
+
 app.get('/', function(req, res) {
     res.render('index.html');
   });
+
+app.listen( port ) ;
+console.log( 'Listening on port %s...', port ) ;
 
