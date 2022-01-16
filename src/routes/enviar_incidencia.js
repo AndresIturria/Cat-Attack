2eb92996f3aef6c47ffe198ae3e18f675e547cba
gisbxrt <gisbxrt@gmail.com>
2022-01-16 18:59:12 +0100

    cambios extras en css, correcciones formularios, añadido warning.png

--- src/routes/enviar_incidencia.js
+++ src/routes/enviar_incidencia.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const router = express.Router();
+const Incidencia = require("../models/incidencias");
+
 
 router.route('/')
     .get(function(req, res, next){
         res.render('enviar_incidencia.njk');
+    })
+
+    .post(function(req, res, next){
+        //wip
+        incidencia = new Incidencia()
+        res.redirect('/dashboard');
     });
 
 module.exports = router;
